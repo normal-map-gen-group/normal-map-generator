9dccb6f1963eef9052d87f3a2c57cabf9deabc90
bm679912 <whatcoloris@gmail.com>
2022-02-03 11:31:23 -0500

    continuing to fix test

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,9 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-//test('renders rotating image', () => {
-//  render(<App />);
-//  const linkElement = screen.getByText(/uploaded spinner/i);
-//  expect(linkElement).toBeInTheDocument();
-//});
+test('renders rotating image', () => {
+  render(<App />);
+  const linkElement = screen.getByAltText(/uploaded spinner/i);
+  console.log(linkElement)
+  //expect(linkElement).toBeInTheDocument();
+});
