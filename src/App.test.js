0c882a684da249ad016296e1ecc8cffba9d06fef
bm679912 <whatcoloris@gmail.com>
2022-02-03 11:35:52 -0500

    removing test entirely

--- src/App.test.js
+++
@@ -1,9 +0,0 @@
-import { render, screen } from '@testing-library/react';
-import App from './App';
-
-test('renders rotating image', () => {
-  render(<App />);
-  const linkElement = screen.getByAltText(/uploaded spinner/i);
-  console.log(linkElement)
-  //expect(linkElement).toBeInTheDocument();
-});
