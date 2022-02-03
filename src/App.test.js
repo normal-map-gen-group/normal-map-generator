4d640b6135a123c56db9c264f49096b1f9604a2e
bm679912 <whatcoloris@gmail.com>
2022-02-03 11:21:32 -0500

    rewriting App.test.js

--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,8 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+test('renders rotating image', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/uploaded spinner/i);
   expect(linkElement).toBeInTheDocument();
 });
