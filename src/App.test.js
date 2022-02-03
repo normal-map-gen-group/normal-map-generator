4c725a957ec26eabcc4c4516e2802a41388895a1
bm679912 <whatcoloris@gmail.com>
2022-02-03 11:42:30 -0500

    added a trivial test

--- src/App.test.js
+++ src/App.test.js
@@ -1,4 +1,6 @@
 import { render, screen } from '@testing-library/react';
+import React from 'react';
+import ReactDOM from 'react-dom';
 import App from './App';
 
 //test('renders rotating image', () => {
@@ -6,3 +8,8 @@ import App from './App';
 //  const linkElement = screen.getByText(/uploaded spinner/i);
 //  expect(linkElement).toBeInTheDocument();
 //});
+
+it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<App />, div);
+  });
