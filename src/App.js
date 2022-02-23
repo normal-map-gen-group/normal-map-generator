df69ed17efff33e50908c91e31f9452657d6705a
Liana <lianapangburn@gmail.com>
2022-02-23 12:16:13 -0500

    commit 1

---
+++ src/App.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import './App.css';
+import UploadImages from './UploadImages';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <p>
+          
+          <UploadImages></UploadImages>
+          <UploadImages></UploadImages>
+        </p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
