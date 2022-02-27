fe467f56d3ddebae660b6afcd73a255e285cd6d7
bm679912 <whatcoloris@gmail.com>
2022-02-27 15:45:00 -0500

    commiting before merge

--- src/App.js
+++ src/App.js
@@ -1,14 +1,17 @@
 import React from 'react';
 import './App.css';
 import UploadImages from './UploadImages';
+import { Button, Card, Row, Col } from 'react-materialize';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          <UploadImages></UploadImages>
-        </p>
+        <Row>
+          <Col s={2}><Button node="upload" waves="light">UPLOAD</Button></Col>
+          <Col s={2}>&nbsp;</Col>
+          <Col s={2}><Button node="download" waves="light">DOWNLOAD</Button></Col>
+        </Row>
       </header>
     </div>
   );
