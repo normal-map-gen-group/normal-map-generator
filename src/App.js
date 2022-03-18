77dfe842d93a5da842d3ff6b9ce29f83d87f0617
Emir Sahin <emirsah1@live.com>
2022-03-18 13:17:26 -0400

    Seperate img proc code into its own component

--- src/App.js
+++ src/App.js
@@ -1,119 +1,35 @@
-import React, {useRef} from 'react';
+import React, { useState } from 'react';
+
 import './App.css';
+import NrmMapGenCanvas from "./components/norm_map_generator"
+
+//NOTE::Element id/class naming conventions to make our lives easier when writing css.
+//Use all lower case and seperate words with a dash. Example: id="upload-button"
+
 
 function App() {
 
-  const Canvas = useRef()
-  let cv = window.cv
-  const img = new Image()
+  //States
+  const [isImageLoaded, setIsImageLoaded] = useState(false) //State to tell if an img is loaded or not.
 
-  let Vector = function(x, y, z){
-    this.x = x
-    this.y = y
-    this.z = z
-  }
-
-  Vector.prototype.Normalize = function(){
-    let length = Math.sqrt(this.x*this.x + this.y*this.y + this.z*this.z)
-    this.x = this.x/length;
-    this.y = this.y/length;
-    this.z = this.z/length;
-  }
-
-     
-
-
-  const onImgLoad = (event) =>{
-
-    const canvas = Canvas.current
-    const ctx = canvas.getContext("2d")
-
-    if(event.target.files && event.target.files[0]){
-      img.src = URL.createObjectURL(event.target.files[0])
-    }
-
-    img.onload = function(){
-      // ctx.drawImage(img, 0, 0, 500, 500)
-      let src = cv.imread(img);
-      let dstx = new cv.Mat();
-      let dsty = new cv.Mat();
-
-      //grayscale
-      cv.cvtColor(src, src, cv.COLOR_RGB2GRAY, 0);
-
-      let ksize = new cv.Size(2, 2);
-      let anchor = new cv.Point(-1, -1);
-      cv.blur(src, src, ksize, anchor, cv.BORDER_DEFAULT);
-
-      //Steps: Compute Sobel In X, Compute Sobel in Y
-      // R = Sobel X
-      // G = Sobel Y
-      // B = 1.0/strength
-      
-      // //X SOBEL
-      cv.Sobel(src, dstx, -1, 1, 0, 3, 1, 0, cv.BORDER_DEFAULT);
-      cv.imshow(canvas, dstx) //show img on canvas so we can access it
-      let imgData = ctx.getImageData(0, 0, img.width, img.height);
-      let sobelX = imgData.data.slice();
-
-      // //Y SOBEL
-      cv.Sobel(src, dsty, -1, 0, 1, 3, 1, 0, cv.BORDER_DEFAULT);
-      cv.imshow(canvas, dsty) //show img on canvas so we can access it
-
-      imgData = ctx.getImageData(0, 0, img.width, img.height);
-      let sobelY = imgData.data.slice();
-
-      let intensity = 0.01;
-      // let level = 1;
-      imgData = ctx.getImageData(0, 0, img.width, img.height);
-
-      for(let i = 0; i < imgData.data.length; i += 4) {
-      
-        let dX = sobelX[i]
-        let dY = sobelY[i]
-        let dZ = 1.0/intensity
-
-        let vector = new Vector(dX, dY, dZ)
-        vector.Normalize()
-
-        imgData.data[i] = (vector.x * 0.5 + 0.5) * 255.0;
-        imgData.data[i + 1] = (vector.y * 0.5 + 0.5) * 255.0;
-        imgData.data[i + 2] = (vector.z) * 255.0;
-        imgData.data[i + 3] = 255.0;
-
-        // dst[dstOff] = (dX/l * 0.5 + 0.5) * 255.0; 	// red
-				// dst[dstOff+1] = (dY/l * 0.5 + 0.5) * 255.0; 	// green
-				// dst[dstOff+2] = dZ/l * 255.0; 			
-
-      }
-      // let ksize = new cv.Size(3, 3);
-      // let anchor = new cv.Point(-1, -1);
-      // cv.blur(src, src, ksize, anchor, cv.BORDER_DEFAULT);
-
-      // let kdata = [-1,-1,-1,-1,20,-1,-1,-1,-1] ;
-      // let M = cv.matFromArray(3,3, cv.CV_32FC1,kdata);
-      // cv.filter2D(src, src, -1, M, anchor, 0, cv.BORDER_DEFAULT);
-
-
-      ctx.putImageData(imgData, 0, 0, 0, 0, img.width, img.height)
-
-      src.delete(); dstx.delete(); dsty.delete();
-    }
-
-  }
-
+  const download_image = function(){
+      var canvas = document.getElementById("normal-canvas");
+      var anchor = document.createElement("a");
+      anchor.href = canvas.toDataURL("image/png");
+      anchor.download = "NormalMap.png";
+      anchor.click();
+  };
 
   return (
     <div className="App">
       <header className="App-header">
-        <p>
-          {/* <UploadImages></UploadImages> */}
-          <canvas ref={Canvas} width="500" height="500"></canvas>
-          <input id="upload-button" type="file" accept="image/*" onChange={onImgLoad}/>
-        </p>
+        <div>
+          <NrmMapGenCanvas isImageLoaded={setIsImageLoaded}></NrmMapGenCanvas>
+          <button id="download-button" onClick={download_image}>Download Image</button>        
+        </div>
       </header>
     </div>
-  );
+  );  
 }
 
 export default App;
---
+++ src/components/norm_map_generator.jsx
@@ -0,0 +1,145 @@
+import React, { useRef } from 'react';
+
+export default function NrmMapGenCanvas(props) {
+
+    let cv = window.cv //Load opencv.
+    const Canvas = useRef() //React ref to get the canvas.
+    const img = new Image()
+    let baseImg = null //Stores the unprocessed img.
+    let sobelxData = null
+    let sobelyData = null
+
+    //Vector class to store vectors. 
+    let Vector = function (x, y, z) {
+        this.x = x
+        this.y = y
+        this.z = z
+    }
+
+    //Normalizes a given vector.
+    Vector.prototype.Normalize = function () {
+        let length = Math.sqrt(this.x * this.x + this.y * this.y + this.z * this.z)
+        this.x = this.x / length;
+        this.y = this.y / length;
+        this.z = this.z / length;
+    }
+
+    //Generate a normal map from the image loaded on the canvas.
+    //TODO::If not fast enough, convert sobels to img data.
+    function GenerateNormalMap() {
+
+        const canvas = Canvas.current
+        const ctx = canvas.getContext("2d")
+        baseImg = cv.imread(img); //base img
+
+        let srcImg = baseImg; //base img
+        let sobelxCVMat = new cv.Mat();
+        let sobelyCVMat = new cv.Mat();
+        let canvasData = null //
+
+        //Convert to grayscale.
+        cv.cvtColor(srcImg, srcImg, cv.COLOR_RGB2GRAY, 0);
+
+        //Blur image.
+        let ksize = new cv.Size(2, 2);
+        let anchor = new cv.Point(-1, -1);
+        cv.blur(srcImg, srcImg, ksize, anchor, cv.BORDER_DEFAULT);
+
+        //Compute sobel in the X direction
+        cv.Sobel(srcImg, sobelxCVMat, -1, 1, 0, 3, 1, 0, cv.BORDER_DEFAULT);
+        cv.imshow(canvas, sobelxCVMat) //show img on canvas so we can access it
+        canvasData = ctx.getImageData(0, 0, img.width, img.height);
+        sobelxData = canvasData.data.slice();
+
+        //Compute sobel in the Y direction
+        cv.Sobel(srcImg, sobelyCVMat, -1, 0, 1, 3, 1, 0, cv.BORDER_DEFAULT);
+        cv.imshow(canvas, sobelyCVMat) //show img on canvas so we can access it
+        canvasData = ctx.getImageData(0, 0, img.width, img.height);
+        sobelyData = canvasData.data.slice();
+
+        //Two values that can be controlled using sliders.
+        let intensity = 0.01; 
+        let level = 1;
+        
+        canvasData = ctx.getImageData(0, 0, img.width, img.height); //TODO::This step may not be needed.
+
+
+        //Loop through the pixels and calculate the RGB colors. This is where the normal map is "created".
+        for (let i = 0; i < canvasData.data.length; i += 4) {
+
+            let dX = sobelxData[i]
+            let dY = sobelyData[i]
+            let dZ = 1.0 / intensity
+
+            let vector = new Vector(dX, dY, dZ)
+            vector.Normalize()
+
+            canvasData.data[i] = (vector.x/level * 0.5 + 0.5) * 255.0; //red
+            canvasData.data[i + 1] = (vector.y/level * 0.5 + 0.5) * 255.0; //green
+            canvasData.data[i + 2] = (vector.z/level) * 255.0; //blue
+            canvasData.data[i + 3] = 255.0;
+        }
+
+        ctx.putImageData(canvasData, 0, 0, 0, 0, img.width, img.height)
+        srcImg.delete(); sobelxCVMat.delete(); sobelyCVMat.delete();
+
+        //Experimental blur code.
+        // let ksize = new cv.Size(3, 3);
+        // let anchor = new cv.Point(-1, -1);
+        // cv.blur(src, src, ksize, anchor, cv.BORDER_DEFAULT);
+        // let kdata = [-1,-1,-1,-1,20,-1,-1,-1,-1] ;
+        // let M = cv.matFromArray(3,3, cv.CV_32FC1,kdata);
+        // cv.filter2D(src, src, -1, M, anchor, 0, cv.BORDER_DEFAULT);
+    }
+
+
+    function updateNormalMap(intensity, level){
+        const canvas = Canvas.current
+        const ctx = canvas.getContext("2d")
+
+        let canvasData = ctx.getImageData(0, 0, img.width, img.height); //TODO::This step may not be needed.
+
+         //Loop through the pixels and calculate the RGB colors. This is where the normal map is "created".
+         for (let i = 0; i < canvasData.data.length; i += 4) {
+
+            let dX = sobelxData[i]
+            let dY = sobelyData[i]
+            let dZ = 1.0 / intensity
+
+            let vector = new Vector(dX, dY, dZ)
+            vector.Normalize()
+
+            canvasData.data[i] = (vector.x/level * 0.5 + 0.5) * 255.0; //red
+            canvasData.data[i + 1] = (vector.y/level * 0.5 + 0.5) * 255.0; //green
+            canvasData.data[i + 2] = (vector.z/level) * 255.0; //blue
+            canvasData.data[i + 3] = 255.0;
+        }
+
+    }
+
+
+    //Entry point.
+    //This function gets called when an image is loaded.
+    const onImgLoad = (event) => {
+
+        //Converts the loaded "thing" into an img.
+        if (event.target.files && event.target.files[0]) {
+            img.src = URL.createObjectURL(event.target.files[0])
+        }
+
+        //onload is used to make sure that the img is fully loaded before any processing.
+        img.onload = function () {
+            props.isImageLoaded(true)
+            GenerateNormalMap()
+        }
+    }
+
+
+    //HTML elements of this component.
+    return (
+        <div id="canvas-container">
+            <canvas id="normal-canvas" ref={Canvas} width="250" height="250"></canvas>
+            <input id="upload-button" type="file" accept="image/*" onChange={onImgLoad} />
+        </div>
+    )
+}
