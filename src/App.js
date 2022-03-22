03b339a9c299f41395c98cea5f2b1cec68833db7
whatcoloris <whatcoloris@gmail.com>
2022-03-22 16:16:14 -0400

    moved DownloadButton to its own jsx file

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React, { useState } from 'react';
 
 import './App.css';
 import NrmMapGenCanvas from "./components/norm_map_generator"
+import DownloadButton from './components/downloadButton';
 
 //NOTE::Element id/class naming conventions to make our lives easier when writing css.
 //Use all lower case and seperate words with a dash. Example: id="upload-button"
@@ -11,14 +12,6 @@ function App() {
 
   //States
   const [isImageLoaded, setIsImageLoaded] = useState(false) //State to tell if an img is loaded or not.
-
-  const download_image = function(){
-      var canvas = document.getElementById("normal-canvas");
-      var anchor = document.createElement("a");
-      anchor.href = canvas.toDataURL("image/png");
-      anchor.download = "NormalMap.png";
-      anchor.click();
-  };
 
   return (
     
@@ -30,9 +23,8 @@ function App() {
       <p style={{textAlign:'center'}}></p>
 
         <div>
-          
           <p><NrmMapGenCanvas isImageLoaded={setIsImageLoaded}></NrmMapGenCanvas>
-          <button style={{color: 'white'}} id="download-button" onClick={download_image}>Download Image</button></p>    
+          <DownloadButton></DownloadButton></p>
         </div>
       </header>
     </div>
---
+++ src/components/downloadButton.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+
+export default function DownloadButton(props) {
+
+  const download_image = function(){
+      var canvas = document.getElementById("normal-canvas");
+      var anchor = document.createElement("a");
+      anchor.href = canvas.toDataURL("image/png");
+      anchor.download = "NormalMap.png";
+      anchor.click();
+  }
+
+  return (
+      <div>
+        <button style={{color: 'white'}} id="download-button" onClick={download_image}>Download Image</button>
+      </div>
+  )
+}
