e349c8641d8981f4cc2d114fff64e1ee1be05a3a
Kiran Aziz <kiransaziz@gmail.com>
2022-03-26 15:14:08 -0400

    Sliders

--- src/App.js
+++ src/App.js
@@ -1,16 +1,31 @@
 import React from 'react';
 import './App.css';
 import UploadImages from './UploadImages';
+import styled from 'styled-components';
+import Slider from './Slider';
+
+const Styles = styled.div`
+
+`;
+
 
 function App() {
   return (
+    <Styles>
     <div className="App">
       <header className="App-header">
         <p>
           <UploadImages></UploadImages>
+          <div className="Slider">
+            <Slider></Slider>
+            <Slider></Slider>
+            <Slider></Slider>
+            <Slider></Slider>
+          </div>
         </p>
       </header>
     </div>
+    </Styles>
   );
 }
 
