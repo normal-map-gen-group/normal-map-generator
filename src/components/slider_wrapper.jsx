f6415fa5ee7cb8df9f4d6ce669b9dc17d8912f26
Talha Qureshi <bmw215t@gmail.com>
2022-04-15 11:35:14 -0400

    Revert "added the name of slider to the wrapper rather than having them float outside"
    This reverts commit 0b419ee91f9d6c5c8d1d705ee172982690bfb9b6.

--- src/components/slider_wrapper.jsx
+++ src/components/slider_wrapper.jsx
@@ -3,25 +3,20 @@ import PropTypes from 'prop-types';
 
 const SliderWrapper = props => {
     return (
-    <div>
-        {props.name_value}
-        <input 
-            className="slider-wrapper"
-            type="range"
-            {...props}
-            min={props.min_value}
-            max={props.max_value}
-            step={props.step_value}
-            defaultValue={props.default_value}
-            onChange={props.funcForThis}
-        />
-    </div>
+    <input 
+        type="range"
+        {...props}
+        min={props.min_value}
+        max={props.max_value}
+        step={props.step_value}
+        defaultValue={props.default_value}
+        onChange={props.funcForThis}
+    />
     );
 };
 
 SliderWrapper.propTypes = {
     children: PropTypes.node,
-    name_value: PropTypes.string,
     min_value: PropTypes.number,
     max_value: PropTypes.number,
     step_value: PropTypes.number,
--- src/components/upload_button.jsx
+++ src/components/upload_button.jsx
@@ -5,10 +5,7 @@ import '../css/upload_button.css';
 const UploadButton = (props) => {
 
   return (
-    <div>
-      Upload Image:
-      <input style={{ color: props.color }} id="upload-button" type="file" accept="image/*" onChange={props.changeFunc} />
-    </div>
+    <input style={{ color: props.color }} id="upload-button" type="file" accept="image/*" onChange={props.changeFunc} />
   )
 };
 
